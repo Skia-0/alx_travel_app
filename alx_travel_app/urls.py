0c8ca682e7045ea11252a4e84c9c41fdc534f41c
Courage Kemawor <kemaworc00@gmail.com>
2025-11-02 15:06:34 +0000

    Create urls.py

---
+++ alx_travel_app/urls.py
@@ -0,0 +1,23 @@
+# alx_travel_app/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ALX Travel App API",
+      default_version="v1",
+      description="API documentation for alx_travel_app",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("listings.urls")),  # create listings/urls.py later
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
